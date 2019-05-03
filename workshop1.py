78db2e39a25f6efddc83ee1505ed17a6df0d15ce
Bryce Snell <bsnell@iastate.edu>
2019-05-03 01:18:13 -0500

    Attempting a drawing function

--- workshop1.py
+++ workshop1.py
@@ -1,6 +1,7 @@
 import scipy.integrate
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 class Equation:
     '''Equation object, it holds the equation, and it's high and low points
@@ -13,8 +14,9 @@ class Equation:
 n = 100  # Number of constants to find
 length = np.pi  # Length of the rod
 constants = np.zeros(n)  # Holds the constants that we output
-equations = [Equation("(20 - ((10*x)/(np.pi/2)))", np.pi/2, np.pi), Equation("(10*x)/(np.pi/2)", 0, np.pi/2)]
-
+equations = [Equation("((20/np.pi)*x)", 0, np.pi/2), Equation("((-20/np.pi)*x+20)", np.pi/2, np.pi)]
+maxTime = 20
+timeStep = 0.1
 
 # Create constants for all equations
 for equation in equations:
@@ -24,17 +26,31 @@ for equation in equations:
         constants[i-1] += (area * (1/length) * 2)  # Add the constants after transforming them by the 2 and 1/length
 
 # Arrays used to hold the xy pairs
-xValues = np.arange(0, length, 0.1)
+xValues = np.arange(0, length, length/n)
 yValues = np.zeros(xValues.size)  # Initialize this to zero
 
+t = 0
+
+fig = plt.gcf()
+fig.show()
+fig.canvas.draw()
+
 # Create the xy pairs
-for yIndex, x in enumerate(xValues):
-    for xIndex, constant in enumerate(constants, 1):
-        yValues[yIndex] += (constant * np.sin((xIndex * np.pi * x) / length))  # Get the sum of sines
+while t < maxTime:
+    for yIndex, x in enumerate(xValues):
+        for xIndex, constant in enumerate(constants, 1):
+            yValues[yIndex] += (constant * np.sin((xIndex * np.pi * x) / length) * np.exp((-(np.pi**2)*(xIndex**2)*t)/length**2))  # Get the sum of sines
+    
+    t += timeStep
+    # Plot and show
+    '''
+    plt.plot(xValues, yValues)
+    plt.show()
+    '''
 
-# Plot and show
-plt.plot(xValues, yValues)
+    plt.plot(xValues, yValues)
+    fig.canvas.draw()
+
 plt.show()
-
 # Print the constants for the user
 print(constants)
